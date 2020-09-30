fc608e43e7acb84e14e97bb739541bd873ab34d1
AndrewMarksArt <andrew.colin.marks@gmail.com>
2020-09-30 15:58:26 -0700

    added section for scraping christies historical records, was able to get everything but it is very messy and shows the need to rethink approach. Christies scrape loads the page, scrolls down, gets detaiils and links, and clicks the previous month button repeating the process untill all months and full auction history is scraped then saved to file.

--- MajorHousesHistoricalRecords.py
+++ MajorHousesHistoricalRecords.py
@@ -55,7 +55,7 @@ class HistoricalAuctionRecords():
 
             # Scroll down to bottom
             for i in range(1, last_height, scroll_speed):
-                self.driver.execute_script(f"window.scrollTo(0, {i+last_height});")
+                self.driver.execute_script(f"window.scrollTo(0, {last_height});")
 
             # Wait to load page randomly between 1 and 10 seconds
             time.sleep(wait)
@@ -256,28 +256,117 @@ class HistoricalAuctionRecords():
         allow_cookies = self.driver.find_element_by_xpath('/html/body/div[1]/div[2]/div[4]/div[2]/div/button')
         allow_cookies.click()
         body = self.driver.find_element_by_css_selector('body')
-        
-        page = 0
 
-        while True:
-            if page == 0:
-                self.scroll_to_bottom(min_wait=1, max_wait=2, scroll_speed=15)
+        xpath = '/html/body/main/div[3]/section/div/chr-calendar/div/section[2]/chr-calendar-list/section/footer/chr-pagination/chr-button[1]/a'
+
+        auction_details = []
+        counter = 0
+        while counter < 241:
+            temp = self.get_christies_details()
+            body.send_keys(Keys.PAGE_UP)
+            time.sleep(1)
+            try:
+                self.scroll_to_bottom(min_wait=1, max_wait=2, scroll_speed=10)
                 body.send_keys(Keys.PAGE_UP)
-                time.sleep(1)
-                button = self.driver.find_element_by_xpath('/html/body/main/div[3]/section/div/chr-calendar/div/section[2]/chr-calendar-list/section/footer/chr-pagination/chr-button/a')
+                site = WebDriverWait(self.driver, 4).until(EC.presence_of_element_located((By.XPATH, '/html/body/main/div[3]/section/div/chr-calendar/div/section[2]/chr-calendar-list/section/footer/chr-pagination/chr-button/a')))
+                button = self.driver.find_element_by_xpath(xpath)
+                time.sleep(3)
                 button.click()
-                page += 1
-                print('click')
-            else:
-                try:
-                    self.scroll_to_bottom(min_wait=1, max_wait=2, scroll_speed=15)
-                    body.send_keys(Keys.PAGE_UP)
-                    time.sleep(1)
-                    button = self.driver.find_element_by_xpath('/html/body/main/div[3]/section/div/chr-calendar/div/section[2]/chr-calendar-list/section/footer/chr-pagination/chr-button[1]/a')
-                    button.click()
-                    print('click')
-                except NoSuchElementException:
-                    break
+            except NoSuchElementException:
+                body.send_keys(Keys.PAGE_DOWN)
+                body.send_keys(Keys.PAGE_DOWN)
+                body.send_keys(Keys.PAGE_DOWN)
+                body.send_keys(Keys.PAGE_DOWN)
+                time.sleep(3)
+                body.send_keys(Keys.PAGE_UP)
+                site = WebDriverWait(self.driver, 4).until(EC.presence_of_element_located((By.XPATH, xpath)))
+                button = self.driver.find_element_by_xpath(xpath)
+                time.sleep(3)
+                button.click()
+
+        
+            auction_details.append(temp)
+            
+            counter += 1
+            print(counter)
+
+        self.driver.quit()
+
+        with open('christies_test', "w", encoding='UTF-8', newline='') as f:
+            writer = csv.writer(f)
+            for row in auction_details:
+                for sale in row:
+                    writer.writerow(sale)
+
+
+
+    def get_christies_details(self):
+        """
+
+        """
+
+        # initialize list that will hold results
+        auction_info = []
+
+        # using try / finally, we want to wait for the content to load
+        # if the content doesn't load after the wait time we will close the driver
+        try:
+            # wait 5 seconds or until the content we are looking for loads
+            site = WebDriverWait(self.driver, 4).until(
+                EC.presence_of_element_located((By.ID, "calendar-tab"))
+            )
+
+            # site loads results dynamically as we scroll so scroll to bottom of page
+            self.scroll_to_bottom(min_wait=1, max_wait=2, scroll_speed=20)
+            
+            # find list items from the section we are interested in and create
+            # a list of elements with the HTML tag we are looking for
+            sales = site.find_elements_by_tag_name("li")
+
+            # loop through the list items we have scraped
+            for sale in sales:
+                # find the content we need from the list items
+                auctions = sale.find_elements_by_class_name("container-fluid")
+                
+                # loop through the list of specific content
+                for auction in auctions:
+
+                    # look for auction title, if not there set to " "
+                    try:
+                        title = auction.find_element_by_tag_name("h4").text
+                    except NoSuchElementException:
+                        title = " "
+                        
+                    # look for the date/time/location of the auction or set to " "
+                    try:
+                        date = auction.find_element_by_xpath("/html/body/main/div[3]/section/div/chr-calendar/div/section[2]/chr-calendar-list/section/chr-events-range/chr-viz-sensor/div/ul/li[1]/chr-event-tile/section/div[1]/span/span[2]").text
+                        location = auction.find_element_by_xpath("/html/body/main/div[3]/section/div/chr-calendar/div/section[2]/chr-calendar-list/section/chr-events-range/chr-viz-sensor/div/ul/li[1]/chr-event-tile/section/div[2]/div[2]/span").text
+                        auction_date = (date + ' ' + location).replace('\n', ' ')
+                    except NoSuchElementException:
+                        auction_date = " "
+
+                    # look for the link to the auction results or set to " "
+                    try:
+                        link_element = auction.find_element_by_tag_name("a")
+                        link = link_element.get_attribute("href")
+                    except NoSuchElementException:
+                        link = " "
+
+                    # create a temp list with information for this auction
+                    temp = [title, auction_date, link]
+
+                    # append the temp list with current information to all auction
+                    # information list that holds all resutls
+                    auction_info.append(temp)
+                    
+            # close the driver once we have the information we need
+            #self.driver.quit()
+
+        except:
+            pass
+
+        return auction_info
+
 
 
 sothebys = 'https://www.sothebys.com/en/results?from=&to=&f2=00000164-609b-d1db-a5e6-e9ff01230000&f2=00000164-609b-d1db-a5e6-e9ff08ab0000&f3=LIVE&f3=ONLINE&q='
