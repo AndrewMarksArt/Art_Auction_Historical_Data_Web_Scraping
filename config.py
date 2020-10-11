ed110c30768dc9c33bebea70ef6ecee9c3c7966e
AndrewMarksArt <andrew.marks@cgu.edu>
2020-10-11 10:42:54 -0700

    first path scraping list of Phillips auctions.
    used list of auction to scrape links to eaach lot that was sold, looks like site layout was changed for most of the auction, got 80 of 270 auctions, need to look at the 190 left to see how the site is structred. once all/majority of auctions are scraped need to scrape each lot.

--- config.py
+++ config.py
@@ -1,7 +1,17 @@
-# imports
+"""
+Set chrome driver path for Selenium to open chrome browser
+
+Set Data paths for each house, all sales lists, lot details, 
+images and results
+"""
 
 # set chrome driver path
 CHROME_PATH = 'C:/Program Files (x86)/chromedriver.exe'
 
-# set path to csv folder
-CSV_PATH = 'data_files/'
+# Data Paths
+DATA_PATH = 'data_files/'
+PHILLIPS = DATA_PATH + 'Phillips/'
+PHILLIPS_LOTS = PHILLIPS + 'Lot_details/'
+PHILLIPS_LOTS_IMAGES = PHILLIPS_LOTS + 'Images/'
+PHILLIPS_LOTS_CSV = PHILLIPS_LOTS + 'CSV_files/'
+PHILLIPS_LOTS_JSON = PHILLIPS_LOTS + 'JSON_files/'
--- phillips_auction_details.py
+++ phillips_auction_details.py
@@ -19,7 +19,7 @@ def get_auction_links(filename):
     """
 
     # set path to phillips auctions csv file
-    FILE_PATH = config.CSV_PATH + filename
+    FILE_PATH = config.PHILLIPS + filename
 
     # initialize list for auciton urls
     auction_links = []
@@ -29,37 +29,80 @@ def get_auction_links(filename):
         reader = csv.reader(f)
         for row in reader:
             # each row has title, date/time, url. Append url to list
-            auction_links.append(row[2])
+            auction_links.append(row)
 
     # return list of auction urls
     return auction_links
 
 
+def scroll_to_bottom():
+    """
+    scroll to bottom of page
+    """
+    # Get scroll height
+    last_height = driver.execute_script("return document.body.scrollHeight")
+
+    while True:
+        # Scroll down to bottom
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        # Wait to load page
+        time.sleep(2)
+
+        # Calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+
+def close_cookies():
+    """
+    When we first open the web browser find and accept cookies button
+    """
+    # wait 5 seconds or until the accept cookies button appears
+    try:
+        WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "alert-button"))
+        )
+
+        # find the accept cookies button and click accept
+        cookies_btn = driver.find_element_by_class_name("alert-button")
+        cookies_btn.click()
+
+        # let us know if the button was clicked
+        print("we accepted cookies and clicked the button")
+
+        # wait for 3 seconds
+        time.sleep(3)
+
+    # quit if cookies button not found
+    except NoSuchElementException:
+        # let us know if we couldn't find the button
+        print("couldn't find the button")
+        # close the driver
+        driver.close()
+
+
+
 # urls of auctions
-list_auctions = get_auction_links('Phillips_Auctions.csv')
+list_auctions = get_auction_links('all_sales_auction_list.csv')
 
 # get url list of Phillips auctions
 driver = webdriver.Chrome(config.CHROME_PATH)
 
-# go to url at indext [_]
-driver.get(list_auctions[0])
+no_grid = []
+different_layout = []
 
-# wait 5 seconds or until the accept cookies button appears
-try:
-    site = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.CLASS_NAME, "alert-button"))
-    )
+for i in range(len(list_auctions)):
+    # go to url at index[i]
+    url = list_auctions[i][-1]
+    print(url)
 
-    # find the accept cookies button and click accept
-    cookies_btn = driver.find_element_by_class_name("alert-button")
-    cookies_btn.click()
-
-    # let us know if the button was clicked
-    print("we accepted cookies and clicked the button")
-
-    # wait for 3 seconds
-    time.sleep(3)
-
+    driver.get(url)
+    if i == 0:
+        close_cookies()
+    
     # find the number of auction lots
     try:
         # header for auction results holds number of lots
@@ -72,32 +115,63 @@ try:
     except NoSuchElementException:
         print("could not find number of lots")
 
-    # find grid that holds oll the lots
-    grid = driver.find_element_by_class_name("standard-grid")
-    lots = grid.find_elements_by_tag_name("li")
-    
-    urls = []
+    try:
+        # find the grid that holds all the lots
+        grid = driver.find_element_by_class_name("standard-grid")
 
-    for lot in lots:
-        link_tag = lot.find_element_by_tag_name("a")
-        url = link_tag.get_attribute("href")
-        urls.append(url)
+        try:
+            # find each list element that links to each lot with detials
+            lots = grid.find_elements_by_tag_name("li")
 
-    for link in urls:
-        print(link)
+            if len(lots) < num_lots:
+                scroll_to_bottom()
 
-    # lot = driver.find_element_by_class_name("phillips-lot")
-    # lot.click()
+                print('Scrolling')
 
-    # wait for 3 seconds then close the driver
-    time.sleep(3)
-    driver.close()
+                # wait
+                time.sleep(2)
 
-# quit if cookies button not found
-except NoSuchElementException:
-    # let us know if we couldn't find the button
-    print("couldn't find the button")
-    # close the driver
-    driver.close()
+                # find the grid that holds all the lots
+                grid = driver.find_element_by_class_name("standard-grid")
+                # find each list element that links to each lot with detials
+                lots = grid.find_elements_by_tag_name("li")
+
+            # initialize an empty list that will hold urls to the lot details
+            urls = []
+
+            # loop through the list elements of lots to find the links to all
+            # of the lot details
+            for lot in lots:
+                link_tag = lot.find_element_by_tag_name("a")
+                url = link_tag.get_attribute("href")
+                urls.append(url)
+
+            with open(f'{config.PHILLIPS_LOTS_CSV} {list_auctions[i][0]} {list_auctions[i][1]}_{str(i)}', "w", encoding='UTF-8', newline='') as f:
+                    writer = csv.writer(f)
+                    for row in urls:
+                        writer.writerow([row])
+
+        except:
+            print("Found Grid but no list of results")
+            no_grid.append(f'{list_auctions[i][0]} {list_auctions[i][1]}_{i}')
+
+    except:
+        print("Different auction results layout -- no grid and no results")
+        different_layout.append(f'{list_auctions[i][0]} {list_auctions[i][1]}_{i}')
+
+    # wait for 3 seconds
+        time.sleep(3)
+
+# close the driver
+driver.close()
+
+with open(f'{config.PHILLIPS_LOTS_CSV} no_gird', "w", encoding='UTF-8', newline='') as f:
+    writer = csv.writer(f)
+    for row in no_grid:
+        writer.writerow([row])
 
 
+with open(f'{config.PHILLIPS_LOTS_CSV} different_layout', "w", encoding='UTF-8', newline='') as f:
+    writer = csv.writer(f)
+    for row in different_layout:
+        writer.writerow([row])
